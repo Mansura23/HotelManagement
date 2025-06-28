d431e829c0dab5aa14b8f109323ab2660c109ec1
Mansura <badalovamansura40@gmail.com>
2025-06-28 17:59:40 +0400

    contreller added

---
+++ src/main/java/az/springdemo/hotelmanagement/controller/HotelController.java
@@ -0,0 +1,45 @@
+package az.springdemo.hotelmanagement.controller;
+
+import az.springdemo.hotelmanagement.entity.Hotel;
+import az.springdemo.hotelmanagement.service.HotelService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/hotels")
+public class HotelController {
+    private final HotelService hotelService;
+
+    public HotelController(HotelService hotelService) {
+        this.hotelService = hotelService;
+    }
+
+
+    @GetMapping
+    public List<Hotel> getHotels() {
+        return hotelService.getAllHotels();
+    }
+    @GetMapping(path = "/{id}")
+    public Hotel getHotelById(@PathVariable Long id) {
+        return hotelService.getHotelById(id);
+    }
+    @PostMapping
+    public Hotel saveHotel(@Valid @RequestBody Hotel hotel) {
+        return hotelService.save(hotel);
+    }
+    @PutMapping(path = "/{id}")
+    public Hotel updateHotel(@Valid @PathVariable Long id,@RequestBody Hotel hotel) {
+       return hotelService.update(id, hotel);
+    }
+    @DeleteMapping(path = "/{id}")
+    public void deleteHotel(@PathVariable Long id) {
+        hotelService.delete(id);
+
+    }
+
+
+
+}
